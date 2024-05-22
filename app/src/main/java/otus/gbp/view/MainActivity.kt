e17a779a46542322f34394a22d4d687caf3cb736
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-05-22 10:14:12 +0200

    View access and state change

--- app/src/main/java/otus/gbp/view/MainActivity.kt
+++ app/src/main/java/otus/gbp/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package otus.gbp.view
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupChange()
+    }
+
+    private fun setupChange() {
+        val change: Button = findViewById(R.id.change)
+        change.setOnClickListener {
+            val text: TextView = findViewById(R.id.text)
+            text.text = "Changed text"
+        }
     }
 }
