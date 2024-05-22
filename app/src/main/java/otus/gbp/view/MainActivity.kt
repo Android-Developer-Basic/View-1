7e27e69fc44a8e270a9ebd690bddc31e8f00937b
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-05-22 10:27:30 +0200

    Creating view programmatically

--- app/src/main/java/otus/gbp/view/MainActivity.kt
+++ app/src/main/java/otus/gbp/view/MainActivity.kt
@@ -1,26 +1,37 @@
 package otus.gbp.view
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupChange()
+        replaceText()
     }
 
-    private fun setupChange() {
+    private fun replaceText() {
         val change: Button = findViewById(R.id.change)
         change.setOnClickListener {
-            val text: TextView = findViewById(R.id.text)
-            text.text = "Changed text"
+            val main: ViewGroup = findViewById(R.id.main)
+            val textIndex = main.indexOfChild(findViewById(R.id.text))
+            main.removeViewAt(textIndex)
+
+            val newText: TextView = TextView(this).apply {
+                text = "Changed text"
+                id = R.id.text
+                layoutParams = LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+            }
+
+            main.addView(newText, textIndex)
         }
     }
 }
